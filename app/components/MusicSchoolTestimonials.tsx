c3a101f0ae74357853792b4e1838ed9866406364
Yashvant Yadav <yashvanty@gmail.com>
2024-02-27 10:18:22 +0530

    Grid effect added

--- app/components/MusicSchoolTestimonials.tsx
+++ app/components/MusicSchoolTestimonials.tsx
@@ -36,7 +36,7 @@ const musicSchoolTestimonials = [
 
 function MusicSchoolTestimonials() {
     return (
-        <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+        <div className="h-[40rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2]  relative flex flex-col items-center justify-center overflow-hidden">
             <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
             <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
                 <div className="w-full max-w-6xl">
